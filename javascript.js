2e34b2ac0feadb62a76082586b2c51dab9ae8b96
prateekanand37 <88392232+prateekanand37@users.noreply.github.com>
2021-08-23 22:37:58 +0530

    js done

---
+++ javascript.js
@@ -0,0 +1,6 @@
+var translatebtn= document.querySelector("#btn");
+var txtInput=document.querySelector("#text-input")
+translatebtn.addEventListener( "click" , clickhandler)
+     function clickhandler(){
+         console.log(txtInput.value);
+         }
